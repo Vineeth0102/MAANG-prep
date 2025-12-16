dc631312907a6d6d4ae21c5c6b1fe14b3e896c45
vineeth shettigar <vineethshettigar0102@gmail.com>
2025-12-16 07:50:13 +0530

    last one pending

--- Striver A - Z sheet/Learn the basics/build-up logical thinking/pattern18/pattern18.py
+++ Striver A - Z sheet/Learn the basics/build-up logical thinking/pattern18/pattern18.py
@@ -1,6 +1,6 @@
 n =  int(input("Enter the number : "))
 
-for i in range(1, n+1) :
-    for j in range(n, i, -1) :
+for i in range(n, 0, -1) :
+    for j in range(i, n + 1) :
         print(chr(65 + j - 1), end=' ')
     print()
