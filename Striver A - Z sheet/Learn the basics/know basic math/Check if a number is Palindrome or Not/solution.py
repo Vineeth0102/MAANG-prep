8fa558db26d1a15629205d4da703e4f22c496998
vineeth shettigar <90184123+Vineeth0102@users.noreply.github.com>
2025-12-16 17:33:58 +0530

    started basic math

---
+++ Striver A - Z sheet/Learn the basics/know basic math/Check if a number is Palindrome or Not/solution.py
@@ -0,0 +1,15 @@
+def pali_check(num: int) -> int:
+    rev = 0
+    copy = num
+    while num != 0:
+        rev = rev * 10 + num % 10
+        num //= 10
+
+    return rev == copy
+
+num = int(input("Enter the numbr : "))
+if pali_check(num):
+    print("The given number is pali")
+else:
+    print("The given number is not pali")
+
