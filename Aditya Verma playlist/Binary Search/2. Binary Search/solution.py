0a091e82bd9f807f611db33052768bdd492bf2d8
vineeth shettigar <vineethshettigar0102@gmail.com>
2026-01-18 19:03:51 +0530

    Binary search Asc and Desc and Order Unknow

---
+++ Aditya Verma playlist/Binary Search/2. Binary Search/solution.py
@@ -0,0 +1,17 @@
+def binarySearch(arr: list[int], target: int) -> int:
+    first, last = 0, len(arr) - 1
+    while first <= last:
+        mid = first + (last - first) // 2  
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            first = mid + 1
+        else:
+            last = mid - 1
+    return -1
+
+arr = list(map(int, input().split()))
+target = int(input())
+print(binarySearch(arr, target))
+
+
---
+++ Aditya Verma playlist/Binary Search/4. Order Not Know Search/solution.py
@@ -0,0 +1,28 @@
+def binarySearch(arr: list[int], target: int) -> int:
+    first, last = 0, len(arr) - 1
+    isAsc = arr[first] < arr[last]
+    if isAsc == True:
+        while first <= last:
+            mid = first + (last - first) // 2  
+            if arr[mid] == target:
+                return mid
+            elif arr[mid] < target:
+                first = mid + 1
+            else:
+                last = mid - 1
+    else:
+        while first <= last:
+            mid = first + (last - first) // 2  
+            if arr[mid] == target:
+                return mid
+            elif arr[mid] > target:
+                first = mid + 1
+            else:
+                last = mid - 1
+    return -1
+
+arr = list(map(int, input().split()))
+target = int(input())
+print(binarySearch(arr, target))
+
+
